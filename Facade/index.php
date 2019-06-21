b0b90717afcd84973cd2b510705419e5b536d8a6
MAX <maxsite.org@gmail.com>
2019-06-21 10:56:39 +0300

    Facade

---
+++ Facade/index.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Design pattern "Facade" (Structural) or "Delegation" (Fundamental)
+ * This is demo code
+ * See for details: http://maxsite.org/page/php-patterns
+ */
+
+/**
+ * Unit 1
+ */
+class Unit1
+{
+    public function run()
+    {
+        echo 'Unit1 run <br>';
+    }
+}
+
+/**
+ * Unit 2
+ */
+class Unit2
+{
+    public function show()
+    {
+        echo 'Unit2 show <br>';
+    }
+}
+
+/**
+ * Unit 3
+ */
+class Unit3
+{
+    public function out()
+    {
+        echo 'Unit3 out <br>';
+    }
+}
+
+/**
+ * Facade start all units
+ */
+class Facade
+{
+    protected $unit1;
+    protected $unit2;
+    protected $unit3;
+
+    public function __construct()
+    {
+        $this->unit1 = new Unit1();
+        $this->unit2 = new Unit2();
+        $this->unit3 = new Unit3();
+    }
+
+    public function start()
+    {
+        $this->unit1->run();
+        $this->unit2->show();
+        $this->unit3->out();
+    }
+}
+
+/**
+ * demo
+ */
+
+$f = new Facade();
+$f->start();
+/*
+    Unit1 run
+    Unit2 show
+    Unit3 out
+ */
+
+ 
+# end of file
---
+++ README.md
@@ -0,0 +1,8 @@
+# Software design pattern for PHP
+
+See details on http://maxsite.org/page/php-patterns
+
+* "Facade" (Structural) / "Delegation" (Fundamental)
+
+
+(c) MaxSite.org, 2019, http://maxsite.org/
